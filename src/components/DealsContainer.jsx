a3d54549d1251384c40fc1f573a681b80d3d05b8
maturaofficial <maturaofficial@gmail.com>
2023-01-20 02:08:40 +0800

    Helped : Added Links to different Components

---
+++ src/components/DealsContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DealsContainer = () => {
+    return (
+        //Your Code for store will begin here 
+        <div>DealsContainer</div>
+    )
+}
+
+export default DealsContainer
