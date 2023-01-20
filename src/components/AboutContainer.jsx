e0e7922c62bff99151ba0336c4d793234a026c57
jeromesky30 <jtm7124@gmail.com>
2023-01-20 22:07:52 +0800

    pull

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -8,7 +8,7 @@ const AboutContainer = () => {
         <div>about us</div>
         <div>You can choose your favorite meal!</div>
         <div>What are you waiting for?</div>
-        <div>What are you waiting for?</div>
+        
         
         </center>
         </section> 
