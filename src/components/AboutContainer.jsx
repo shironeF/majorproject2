11a95942492716bb545b88effc03897123de1bfa
jeromesky30 <jtm7124@gmail.com>
2023-01-20 21:55:25 +0800

    commit

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -9,7 +9,7 @@ const AboutContainer = () => {
         <div>You can choose your favorite meal!</div>
         <div>What are you waiting for?</div>
         <div>What are you waiting for?</div>
-        <div>What are you waiting for?</div>
+        
         </center>
         </section> 
       
