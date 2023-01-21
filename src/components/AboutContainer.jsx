bb75c10ddc57c32634864a6d0ad01fc84c8340de
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-01-21 16:27:35 +0800

    started createcontainer

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,16 +4,16 @@ const AboutContainer = () => {
     return (
 
         <section>
-        <center>
-        <div>about us</div>
-        <div>You can choose your favorite meal!</div>
-        <div>What are you waiting for?</div>
-        <div>What are you waiting for?</div>
-        
-        </center>
-        </section> 
-      
-        
+            <center>
+                <div>about us</div>
+                <div>You can choose your favorite meal!</div>
+                <div>What are you waiting for?</div>
+                <div>What are you waiting for?</div>
+
+            </center>
+        </section>
+
+
     )
 
 }
--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion';
 
 const CreateContainer = () => {
+
+    const [title, setTitle] = useState("");
+    const [calories, setCalories] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategories] = useState("null");
+    const [imageAsset, setImageAsset] = useState("null");
+    const [field, setFields] = useState("false");
+    const [alertStatus, setAlertStatus] = useState("s");
+    const [msg, setMsg] = useState("null");
+    const [isLoading, setIsLoading] = useState("false");
+
     return (
-        <div>CreateContainer</div>
+        <div className='w-full min-h-screen flex items-center justify-center'>
+            <div className='w-[90%] md:w-[75%] border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center'>
+                {
+                    field && (
+                        <p className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${alertStatus === 'danger' ? "bg-red-400 text-red-800" : "bg-emerald-400 text-emerald-800"}`}>
+                            {msg}
+                        </p>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
