a85f2a9417864c15682f539a6d20eccc2fe3a885
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-01-25 01:31:52 +0800

    added category tabs

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoFastFood } from 'react-icons/io5'
+import { category, categories } from '../utils/data'
 
 const MenuContainer = () => {
+
+    const [filter, setFilter] = useState('chickens');
+
     return (
-        <section className='w-full my-6' id='menu'>
+        < section className='w-full my-6' id='menu' >
             <div className='w-full flex flex-col items-center justify-center'>
                 <p className='text-2xl font-semibold text-headingColor uppercase relative before:absolute before:rounded-lg before:content before:w-56 before:h-1 before:-bottom-2 before:left-1 before:bg-orange-400 transition-all ease-in-out mr-auto'>
                     Featured stores
                 </p>
 
                 <div className='w-full flex items-center justify-start lg:justify-center gap-8 mt-6 overflow-x-scroll scrollbar-none'>
-                    <div className='group bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center duration-150 transition-all ease-in-out'></div>
+                    {categories && categories.map(category => (
+                        <div key={category.id} className={`group ${filter === category.urlParamName ? 'bg-orange-400' : 'bg-white'} w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center duration-150 transition-all ease-in-out hover:bg-orange-400`}>
+                            <div className={`w-10 h-10 rounded-full ${filter === category.urlParamname ? 'bg-white' : 'bg-orange-400'} group-hover:bg-white flex items-center justify-center`}>
+                                <IoFastFood className="text-textColor group-hover:text-card text-lg" />
+                            </div>
+                            <p className={`text-sm ${filter === category.urlParamname ? 'text-white' : 'text-textColor'} group-hover:text-white`}>{category.name}</p>
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -17,7 +17,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
             {data && data.map(item => (< div key={item.id} className='w-300 min-w-[300px] md:w-340 md:min-w-[340px] h-auto my-12 bg-gradient-to-br from-orange-300 to-orange-400 rounded-lg p-2 shadow-md backdrop-blur-lg hover:drop-shadow-lg' >
                 <div while className='w-full flex items-center justify-between'>
                     <motion.img whileHover={{ scale: 1.2 }} src={item?.imageURL} alt="" className='w-48 -mt-8' />
-                    <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
+                    <motion.div whileTap={{ scale: 0.75 }} className='w-12 h-12 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
                         <MdShoppingBasket className='text-white' />
                     </motion.div>
                 </div>
