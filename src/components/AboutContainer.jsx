39c9fdca3a7e4f68a4a3d458cd4bffd0467c0325
jeromesky30 <jtm7124@gmail.com>
2023-01-26 16:58:40 +0800

    update

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,19 +3,58 @@ import React from 'react'
 const AboutContainer = () => {
   return (
 
-    <section>
-      <center>
-        <div>about us</div>
-        <div>You can choose your favorite meal!</div>
-        <div>What are you waiting for?</div>
-        <div>SDGSDFSDF</div>
-
-      </center>
-    </section>
-
-
+    <div className='row d-flex'>
+    <div class="card-group">
+  <div class="col-4 card ">
+    < img src="/img/bg2.jpg" class="card-img-top" alt="..."/>
+    < img src="/img/ivanprofile.jpg" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 class="card-title">Ma.Ivy Vanessa Nohara</h5>
+      <p class="card-text">Time is Diamond, maiba-iba naman.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="col-4 card">
+    <img src="./img/bg1.jpg" class="card-img-top" alt="..."/>
+    <img src="/img/chrisprofile.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <h5 class="card-title">Chris Jhovin C. Cabral.</h5>
+      <p class="card-text">Pag nanalo ako sa lotto,bibilhin ko ang KodeGO</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="col-4 card">
+    <img src="" class="card-img-top" alt="bg"/>
+    <img src="/img/profile.jpg" class="card-img-top" alt="bg"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <h5 class="card-title">Jerome Pingol</h5>
+      <p class="card-text">Gwapo lang ambag ko sa grupo.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="col-3 card">
+    <img src="" class="card-img-top" alt="profile"/>
+    <img src="/img/ron2.jpg" class="card-img-top" alt="profile"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <h5 class="card-title">Shirone Fortuno</h5>
+      <p class="card-text">Minsan nako nagbuhat ng mabigat ba bagay,pero mas mabigat pala tong grupo nato buhatin kahit hindi bagay.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  </div>
+  </div>
+    
   )
 
 }
 
 export default AboutContainer
+
