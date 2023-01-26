ab1b793e271c15f91251dac173337620bd3aaa4e
Chris jhovin Cabral <chrisjhovincabral25@yahoo.com>
2023-01-27 06:13:24 +0800

    Final commit

--- src/components/DealsContainer.jsx
+++ src/components/DealsContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import MainContainer from './MainContainer';
+import Menu from './Menu';
 import MenuContainer from './MenuContainer';
 
 const DealsContainer = () => {
     return (
         <div>
+          
+            <Menu />
             <MenuContainer />
         </div>
 
---
+++ src/helpers/MenuList.js
@@ -0,0 +1,41 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+
+
+export const MenuList = [
+  {
+    name: "Fortuno Pizza",
+    image: Pepperoni,
+    price: 143.00,
+  },
+  {
+    name: "Jerome Pizza",
+    image: Margherita,
+    price: 69.00,
+  },
+  {
+    name: "Chris Jhovin Special Pizza",
+    image: PedroTechSpecial,
+    price: 420,
+  },
+  {
+    name: "Ivan Pizza",
+    image: Vegan,
+    price: 150,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 250.00,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 20,
+  },
+];
